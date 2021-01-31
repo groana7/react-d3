2231e4a2e623872ae02a383c82cb9774434bdc00
groana <groana7@gmail.com>
2021-01-31 16:32:50 -0500

    refactor: d3 promise

--- example-d3/src/D3Chart.js
+++ example-d3/src/D3Chart.js
@@ -1,8 +1,6 @@
 // this is the D3 class, will not extend from React.Component
 import * as d3 from 'd3';
 
-// dummy data
-const data = [20, 12, 16, 25, 20];
 const url = 'https://udemy-react-d3.firebaseio.com/ages.json';
 
 export default class D3Chart {
@@ -17,34 +15,27 @@ export default class D3Chart {
         .attr('width', 500)
         .attr('height', 500);
 
-    // this returns a promise
-    d3.json(url).then((agesData) => {
-      console.log(agesData)
-      // add more than one shape to the screen at a timeby using a data join
-      const rects = svg.selectAll('rect')
-        .data(agesData);
-  
-      rects
-        .enter()
-        .append('rect')
-          .attr('x', (obj, idx) => idx * 100)
-          .attr('y', 50)
-          .attr('width', 50)
-          .attr('height', (obj) => obj.age * 10) // multiply by 10 to make it larger
-          .attr('fill', (obj) => {
-            return (obj.age > 10) ? 'red' : 'green'
-          });
-    })
+    // unclear on the parenthesis syntax here
+    (async () => {
+      try {
+        const agesData = await d3.json(url);
+        console.log('AGES DATA', agesData);
 
+        const rects = svg.selectAll('rect').data(agesData);
 
-    // messy solution because you can't keep track of which elem needs to update and when
-    // data.forEach((elem, idx) => {
-    //   svg.append("rect")
-    //     .attr('x', idx * 100)
-    //     .attr('y', 50)
-    //     .attr('width', 50)
-    //     .attr('height', elem)
-    //     .attr('fill', 'grey')
-    // })
+        rects
+          .enter()
+          .append('rect')
+            .attr('x', (obj, idx) => idx * 100)
+            .attr('y', 50)
+            .attr('width', 50)
+            .attr('height', (obj) => obj.age * 10) // multiply by 10 to make the bars more visible
+            .attr('fill', (obj) => {
+              return obj.age > 10 ? 'red' : 'green';
+            });
+      } catch (error) {
+        console.error(error);
+      }
+    })();
   }
 }
